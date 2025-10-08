226501b093d5df39fb91424961388c9719e47e25
Theo <chimou0@sjtu.edu.cn>
2025-10-08 19:59:17 +0800

    feat: use dynamic version instead of hardcoded value

--- casbin_cli/__version__.py
+++ casbin_cli/__version__.py
@@ -1,1 +1,21 @@
-__version__ = "1.0.0"
+import subprocess
+
+
+def get_base_tag():
+    try:
+        command = ["git", "describe", "--tags", "--abbrev=0"]
+
+        tag = subprocess.check_output(
+            command, text=True, stderr=subprocess.PIPE
+        ).strip()
+
+        if tag.startswith("v"):
+            return tag[1:]
+        return tag
+
+    except subprocess.CalledProcessError:
+        print("Error: Failed to find any git tags.")
+        return None
+
+
+__version__ = get_base_tag()
